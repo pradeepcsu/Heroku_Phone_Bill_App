238c11e85d66c45a27be9429dde394f3e033d1b5
pradeepcsu <pradeepcsu@gmail.com>
2018-09-29 17:48:22 -0500

    added mysql part

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var mysql = require('mysql');
 var app = express();
 bodyParser = require('body-parser'),
 path = require("path");
@@ -6,8 +7,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 
+var connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'b374f3f0f40afe',
+    password: '72b67f0d',
+    database: 'heroku_7cd88f660d93013'
+});
+
+connection.connect();
+
+
 app.get('/', function(request, response) {
-    response.send('Hello World');
+    connection.query('SELECT * from users1', function(err, rows, fields){
+        if(err){
+            console.log('error: ', err);
+            throw err;
+        }
+    response.send(['Hello World', rows]);
+    });
 });
 
 var port = process.env.PORT || 5000;
