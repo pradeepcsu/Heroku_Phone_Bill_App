b7bf7c17953e8d29a0545e612c0a4a535afe834d
pradeepcsu <pradeepcsu@gmail.com>
2018-09-29 16:38:28 -0500

    updated index

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
+bodyParser = require('body-parser'),
+path = require("path");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
 app.get('/', function(request, response) {
     response.send('Hello World');
 });
@@ -9,3 +14,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
+
