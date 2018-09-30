1c9c462a11f4a0c959904d2c04e286d0cf681549
pradeepcsu <pradeepcsu@gmail.com>
2018-09-30 00:15:17 -0500

    updated server connection

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 
-var connection = mysql.createConnection({
+var db_config = {
     host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'b374f3f0f40afe',
     password: '72b67f0d',
     database: 'heroku_7cd88f660d93013'
-});
+};
 
 // connection.connect();
 
@@ -40,7 +40,7 @@ function handleDisconnect() {
     connection.connect(function(err) {              	// The server is either down
         if (err) {                                     // or restarting (takes a while sometimes).
             console.log('2. error when connecting to db:', err);
-            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+            setTimeout(handleDisconnect, 3000); // We introduce a delay before attempting to reconnect,
         }                                     	// to avoid a hot loop, and to allow our node script to
     });                                     	// process asynchronous requests in the meantime.
     											// If you're also serving http, display a 503 error.
@@ -57,12 +57,12 @@ function handleDisconnect() {
 handleDisconnect();
 
 app.get('/', function(request, response) {
-    connection.query('SELECT * from users1', function(err, rows, fields) {
-        if (err) {
+    connection.query('SELECT * from users1', function(err, rows, fields){
+        if(err){
             console.log('error: ', err);
             throw err;
         }
-        response.send(['Hello World!', rows]);
+    response.send(['Hello World', rows]);
     });
 });
 
