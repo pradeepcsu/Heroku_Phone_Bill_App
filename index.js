0243c7a9aca613d966ab76dc97bb1af15706c84a
pradeepcsu <pradeepcsu@gmail.com>
2018-10-02 20:28:23 -0500

    updated html

--- index.js
+++ index.js
@@ -5,54 +5,78 @@ var connection;
 bodyParser = require('body-parser'),
 path = require("path");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'www')));
 
-  var db_config = {
-    host: 'us-cdbr-iron-east-01.cleardb.net',
-    user: 'b374f3f0f40afe',
-    password: '72b67f0d',
-    database: 'heroku_7cd88f660d93013'
-};
+//   var db_config = {
+//     host: 'us-cdbr-iron-east-01.cleardb.net',
+//     user: 'b374f3f0f40afe',
+//     password: '72b67f0d',
+//     database: 'heroku_7cd88f660d93013'
+// };
+
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host: 'us-cdbr-iron-east-01.cleardb.net',
+  user: 'b374f3f0f40afe',
+  password: '72b67f0d',
+  database: 'heroku_7cd88f660d93013'
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
 
-function handleDisconnect() {
-    console.log('1. connecting to db:');
-    connection = mysql.createConnection(db_config);
-    connection.connect(function(err) {              
-        if (err) {                                  
-            console.log('2. error when connecting to db:', err);
-            setTimeout(handleDisconnect, 3000); 
-        }                                     
-    });                                     	
+// function handleDisconnect() {
+//     console.log('1. connecting to db:');
+//     connection = mysql.createConnection(db_config);
+//     connection.connect(function(err) {              
+//         if (err) {                                  
+//             console.log('2. error when connecting to db:', err);
+//             setTimeout(handleDisconnect, 3000); 
+//         }                                     
+//     });                                     	
     											
-    connection.on('error', function(err) {
-        console.log('3. db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 
-            handleDisconnect();                      	
-        } else {                                      	
-            throw err;                                  
-        }
-    });
-}
+//     connection.on('error', function(err) {
+//         console.log('3. db error', err);
+//         if (err.code === 'PROTOCOL_CONNECTION_LOST') { 
+//             handleDisconnect();                      	
+//         } else {                                      	
+//             throw err;                                  
+//         }
+//     });
+// }
 
 // handleDisconnect();
+
+
+// pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+//   });
+
+
 
 app.post('/getMonthlybill',function(req, res){
 	res.setHeader('Content-Type', 'application/json');
 	  res.header("Access-Control-Allow-Origin", "*");
-//   connection.connect();
-    handleDisconnect();
+//   //connection.connect();
+    // handleDisconnect();
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	// Request methods you wish to allow
     var selectedMonth=req.body.selectedMonth;
     var selectedYear=req.body.selectedYear;
 	console.log(selectedMonth+"    sample ");
     var sql="select * from users1 where month='"+selectedMonth+"' AND year = '"+selectedYear+"'";
-	connection.query(sql, function(err, rows) {
+
+
+	// connection.query(sql, function(err, rows) {
+    pool.query(sql, function (err, rows) {
+        // if (error) throw error;
+        // console.log('The solution is: ', results[0].solution);
+
 		if (err) {
             // connection.end();
 			res.send(JSON.stringify({
@@ -72,37 +96,70 @@ app.post('/getMonthlybill',function(req, res){
 	});
 });
 
-app.get("/",(req, res) => {
-    res.sendFile(__dirname + "/table1.html");
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname + '/table1.html'));
 });
 
 app.post('/getDetailedbill',function(req, res){
 	res.setHeader('Content-Type', 'application/json');
 	  res.header("Access-Control-Allow-Origin", "*");
-//   connection.connect();
-    handleDisconnect();
+//   //connection.connect();
+    // handleDisconnect();
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	// Request methods you wish to allow
     var selectedMonth=req.body.selectedMonth;
     var selectedYear=req.body.selectedYear;
-	console.log(selectedMonth+"    sample ");
+    var selectedName=req.body.selectedName;
+    console.log(selectedName+"    sample ");
+    console.log(selectedMonth+"    sample ");
+	console.log(selectedYear+"    sample ");
+
     var sql="select * from users1 where month='"+selectedMonth+"' AND year = '"+selectedYear+"' AND name = '"+selectedName+"'";
-	connection.query(sql, function(err, rows) {
-		if (err) {
-            // connection.end();
-			res.send(JSON.stringify({
-				data: [],
-				error:err
-			}));
-		} 
-		else 
-		{
-            // connection.end();
-			res.send(JSON.stringify({
-				data: rows,
-				error:""
-            }));
+	// connection.query(sql, function(err, rows) {
+	// 	if (err) {
+    //         // connection.end();
+	// 		res.send(JSON.stringify({
+	// 			data: [],
+	// 			error:err
+	// 		}));
+	// 	} 
+	// 	else 
+	// 	{
+    //         // connection.end();
+	// 		res.send(JSON.stringify({
+	// 			data: rows,
+	// 			error:""
+    //         }));
             
-		}
-	});
+	// 	}
+    // });
+    
+    
+	// connection.query(sql, function(err, rows) {
+        pool.query(sql, function (err, rows) {
+            // if (error) throw error;
+            // console.log('The solution is: ', results[0].solution);
+    
+            if (err) {
+                // connection.end();
+                res.send(JSON.stringify({
+                    data: [],
+                    error:err
+                }));
+            } 
+            else 
+            {
+                // connection.end();
+                res.send(JSON.stringify({
+                    data: rows,
+                    error:""
+                }));
+                
+            }
+        });
+
+
+
+
+
 });
